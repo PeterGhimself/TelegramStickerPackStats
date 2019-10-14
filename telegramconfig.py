40e515519d278d1a5c77557809e18934b16f58a4
PeterGhimself <petergranitski@gmail.com>
2019-10-14 02:18:25 -0400

    Clarifications regarding API token obtainment

--- telegramconfig.py
+++ telegramconfig.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 # this is a template config file
 # use your own data here after getting your dev API tokens
+# you can get your name, api_id, and api_hash here: https://my.telegram.org/apps
+# you do not need to input a URL when creating the account
 user = { 'name': 'someAppName', 'api_id': 'someNumber', 'api_hash': 'someHash'}
