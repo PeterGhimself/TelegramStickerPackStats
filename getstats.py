2f448635b05cb50155884317317cd7f641f0e984
PeterGhimself <petergranitski@gmail.com>
2019-10-14 00:53:14 -0400

    Initial script allows to send messages to contacts
    The message will always be the same "I just sent you this message from my python script :)"
    By default it will send the message to yourself. You may pass a username (must be publicly available) as an argument to the script to set a recipient.

---
+++ getstats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from telethon import TelegramClient
+from telethon import utils
+import telegramconfig as cfg
+import sys
+
+client = TelegramClient(cfg.user['name'], cfg.user['api_id'], cfg.user['api_hash'])
+
+async def main():
+    # if no args passed, send message to myself
+    recipient = 'me'
+
+    if len(sys.argv) >= 2:
+        recipient = sys.argv[1]
+        print('recipient:', recipient)
+        result = await client.get_entity(recipient)
+        display_name = utils.get_display_name(result)
+        if len(display_name) > 0:
+            print("found recipient: " + recipient)
+            print("display name: " + display_name)
+
+    # Now you can use all client methods listed below, like for example...
+    await client.send_message(recipient, 'I just sent you this message from my python script :)')
+
+with client:
+    client.loop.run_until_complete(main())
---
+++ telegramconfig.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# this is a template config file
+# use your own data here after getting your dev API tokens
+user = { 'name': 'someAppName', 'api_id': 'someNumber', 'api_hash': 'someHash'}
