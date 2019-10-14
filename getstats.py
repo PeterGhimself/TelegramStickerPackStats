02a9f40488434701b4c70694ae1376c7bbd83ce2
PeterGhimself <petergranitski@gmail.com>
2019-10-14 03:40:30 -0400

    Two optional args: first one for username, second one for message
    Default args are "me" for user (whoever runs the script) and "Hey (name of person), I just sent you this from a python script :)"

--- getstats.py
+++ getstats.py
@@ -9,18 +9,33 @@ client = TelegramClient(cfg.user['name'], cfg.user['api_id'], cfg.user['api_hash
 async def main():
     # if no args passed, send message to myself
     recipient = 'me'
+    msg = 'I just sent you this from a python script :)'
+    default_msg = False
 
-    if len(sys.argv) >= 2:
+    if len(sys.argv) == 3:
+        msg = sys.argv[2]
         recipient = sys.argv[1]
-        print('recipient:', recipient)
-        result = await client.get_entity(recipient)
-        display_name = utils.get_display_name(result)
-        if len(display_name) > 0:
-            print("found recipient: " + recipient)
-            print("display name: " + display_name)
+        default_msg = True
+    elif len(sys.argv) == 2:
+        recipient = sys.argv[1]
 
-    # Now you can use all client methods listed below, like for example...
-    await client.send_message(recipient, 'I just sent you this message from my python script :)')
+    result = await client.get_entity(recipient)
+    display_name = utils.get_display_name(result)
+
+    if len(display_name) > 0:
+        print('found recipient: ' + recipient + ', display name: ' + display_name)
+        first_name = display_name.split(' ')[0]
+
+        if not default_msg:
+            msg = 'Hey ' + first_name + '! ' + msg
+    else:
+        print('recipient: ' + recipient + ' not found')
+
+
+    print('sending message: "' + msg + '"')
+
+    # send message to recipient
+    await client.send_message(recipient, msg)
 
 with client:
     client.loop.run_until_complete(main())
