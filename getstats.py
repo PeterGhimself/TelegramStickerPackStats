12405545ff6982de0167b4841d2792e620e2ec86
PeterGhimself <petergranitski@gmail.com>
2019-10-15 07:29:41 -0400

    Add /stats (each sticker) data, refactoring, add reminder for bugfix in JSON

--- getstats.py
+++ getstats.py
@@ -2,6 +2,9 @@
 from telethon import TelegramClient
 from telethon import utils, events
 from telethon.tl.types import InputMessagesFilterPhotos
+from telethon.tl.functions.messages import GetAllStickersRequest
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
 import telegramconfig as cfg
 import sys
 import time
@@ -40,6 +43,8 @@ def break_down(msgs):
 
 # these functions parse bot responses messages
 # and arrify them so that they can be converted to json
+#@TODO: CLEAN UP the mess in: packstats.json and top20.json
+# where some values end up as keys instead of values to their corresponding keys
 def get_arr_packstats(msg):
     msgs = msg.split('\n\n')
     # remove first and last
@@ -58,17 +63,47 @@ def get_arr_top20(msg):
     msgs = msg.split('\n')
     return break_down(msgs)
 
-# json-ify the data already in array form
-def get_json(arr):
+def get_arr_stats(msg):
+    msg = str(msg)
+    msgs = msg.split('\n\n')
+    msgs = str(msgs).split('\\n')
+    # acceptance criteria: must be tuple
+    pattern = re.compile('(\w+: \d+)|(\w+:\d+)')
+    print('MSGS', msgs)
+
+    for m in msgs:
+        #print('m', m)
+        if not pattern.match(m):
+            msgs.remove(m)
+
+    print('MSGS post for', msgs)
+
+    # get rid of random '\\'s
+    msgs = list(filter(('\\').__ne__, msgs))
+    print('MSGS post filter', msgs)
+
+    # remove '\\'s within good strings
+    msgs = [item.replace('\\', '') for item in msgs]
+    print('MSGS post replace', msgs)
+
+    return break_down(msgs)
+
+def get_nested_data(arr, token='#'):
     data = {}
+    packs = []
+    is_valid = False
 
-    # case of packtop and top20
-    if '#' in arr[0]:
-        packs = []
+    if token != '#':
+        try:
+            re.compile(token)
+            is_valid = True
+        except re.error:
+            is_valid = False
 
+    if not is_valid:
         # get all packs first
         for l in arr:
-            if '#' in l:
+            if token in l:
                 packs.append(l)
 
         pack_stats = [[] for i in range(len(packs))]
@@ -76,7 +111,7 @@ def get_json(arr):
         # construct nested arrays
         i = -1
         for l in arr:
-            if '#' in l:
+            if token in l:
                 i += 1
             else:
                 pack_stats[i].append(l)
@@ -85,6 +120,41 @@ def get_json(arr):
         for p in packs:
             data[p] = pack_stats[i]
             i += 1
+    else:
+        token = re.compile(token)
+        for l in arr:
+            if token.search(l):
+                packs.append(l)
+
+        stats = [[] for i in range(len(packs))]
+
+        # construct nested arrays
+        i = -1
+        for l in arr:
+            if token.search(l):
+                i += 1
+            else:
+                stats[i].append(l)
+
+        i = 0
+        for p in packs:
+            data[p] = stats[i]
+            i += 1
+
+    return data
+
+# json-ify the data already in array form
+def get_json(arr, token='#'):
+    data = {}
+
+    if token == False:
+        # bypass
+        json_data = json.dumps(arr, indent = 4)
+        return json_data
+
+    # case of packtop and top20
+    if token in arr[0]:
+        data = get_nested_data(arr, token)
     # case of packstats
     else:
         for i in range(0, (len(arr) - 1)):
@@ -92,17 +162,19 @@ def get_json(arr):
             if i % 2 == 0:
                 data[arr[i]] = arr[i + 1]
 
-    json_data = json.dumps(data, indent=4)
+    json_data = json.dumps(data, indent = 4)
 
     return json_data
 
 # helper function to insert unicodes into array for top 20
-def merge_unicodes(arr, unicodes):
+# default offset set to 1 --> unicode inserted after token
+# set to 0 to insert before token
+def merge_unicodes(arr, unicodes, token, offset = 1):
     indices = []
     ctr = 0
     for i in range(0, len(arr) - 1):
-        if '#' in arr[i]:
-            indices.append(i + 1 + ctr) # insert after '#'
+        if token in arr[i]:
+            indices.append(i + offset + ctr) # insert after token
             ctr += 1 # need to shift next index by amount inserted before it
 
     i = 0
@@ -122,6 +194,8 @@ async def get_stats(event):
         replies.append(msg)
     elif '#' in msg:
         replies.append(msg)
+    elif 'Stats for this sticker' in msg:
+        replies.append(msg)
     else:
         rest.append(msg)
         path = await message.download_media()
@@ -130,9 +204,10 @@ async def main():
     global replies
     global rest
     recipient = 'Stickers'
-    queries = ['/packstats', '/packtop 100', '/top 20']
-    jsons = ['packstats.json', 'packtop.json', 'top20.json']
-    sticker_set = 'SpaceConcordia' #@TODO: pass this as arg to script
+    queries = ['/packstats', '/packtop 100', '/top 20', '/stats']
+    jsons = ['packstats.json', 'packtop.json', 'top20.json', 'stats.json']
+    # this is the short_name (not title) attribute of a sticker set object
+    sticker_set_name = 'SpaceConcordia' #@TODO: pass this as arg to script
     client_obj_file = './client_obj.dictionary'
     client_obj = None
 
@@ -161,12 +236,12 @@ async def main():
         print('found recipient: ' + recipient + ', display name not found')
 
     print('sending message: "' + queries[0] + '"')
-
+    
     # send /packstats query
     await client.send_message(recipient, queries[0])
-    time.sleep(0.5)
-    await client.send_message(recipient, sticker_set)
-    time.sleep(0.5)
+    time.sleep(0.3)
+    await client.send_message(recipient, sticker_set_name)
+    time.sleep(0.3)
 
     # synch up
     await client.catch_up()
@@ -185,7 +260,7 @@ async def main():
     print('sending message: "' + queries[1] + '"')
 
     await client.send_message(recipient, queries[1])
-    time.sleep(0.5)
+    time.sleep(0.3)
 
     # synch up again
     await client.catch_up()
@@ -200,7 +275,7 @@ async def main():
     # send /top 20
     print('sending message: "' + queries[2] + '"')
     await client.send_message(recipient, queries[2])
-    time.sleep(0.5)
+    time.sleep(0.3)
 
     # synch up again
     await client.catch_up()
@@ -249,11 +324,74 @@ async def main():
     print('done')
     print('unicodes found:', u_codes)
 
-    top20_stats = merge_unicodes(flatten(top20_stats), u_codes)
+    top20_stats = merge_unicodes(flatten(top20_stats), u_codes, '#')
     json_data = get_json(top20_stats)
 
     print(jsons[2] + ':', json_data)
     print(json_data, file=open(jsons[2], 'w'))
+    print()
+
+    # /stats (per sticker)
+
+    # Get all the sticker sets this user has
+    sticker_sets = await client(GetAllStickersRequest(0))
+
+    # select the chosen one
+    sticker_set = None
+    for s in sticker_sets.sets:
+        if s.short_name == 'SpaceConcordia':
+            sticker_set = s
+            print('sticker_set', sticker_set)
+
+    # Get the stickers for this sticker set
+    stickers = await client(GetStickerSetRequest(
+        stickerset=InputStickerSetID(
+            id=sticker_set.id, access_hash=sticker_set.access_hash
+        )
+    ))
+
+
+    u_codes = []
+    # Stickers are nothing more than files, so send that
+    for sticker in stickers.documents:
+        try:
+            print('sticker', sticker)
+        except Exception as e:
+            err = '"' + str(e) + '"'
+            u_code = err.partition('encode character')[2]
+            u_code = u_code.partition('in position')[0].strip()
+            u_codes.append(u_code)
+
+        await client.send_message(recipient, queries[3])
+        time.sleep(0.3)
+        await client.send_message(recipient, sticker_set_name)
+        time.sleep(0.3)
+
+        await client.send_file(recipient, sticker)
+
+    time.sleep(0.3)
+    # synch up
+    await client.catch_up()
+    print('u_codes', u_codes)
+
+    total_stickers = len(stickers.documents)
+    print('total_stickers', total_stickers)
+    print('len(replies)', len(replies))
+    start_index = len(replies) - total_stickers
+    responses = replies[start_index:]
+    print('start_index', start_index)
+    print('responses', responses)
+    stats = get_arr_stats(replies[start_index:])
+    print('stats', stats)
+
+    stats = merge_unicodes(stats, u_codes, 'Today', 0)
+    json_data = get_json(get_nested_data(stats, '\\\\(u|U)\d+\w*\d*'), False)
+    print('json_data', json_data)
+
+    print('final_stats', stats)
+
+    print(jsons[3] + ':', json_data)
+    print(json_data, file=open(jsons[3], 'w'))
     print()
 
 with client:
